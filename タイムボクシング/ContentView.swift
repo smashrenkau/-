2ab09ed03fa69acd3f4f1fc46eca457a5c4d33b2
keiobewitch-wq <keio.bewitch@gmail.com>
2026-03-11 19:01:50 +0900

    new

---
+++ タイムボクシング/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  タイムボクシング
+//
+//  Created by 奥川皓大 on 2026/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
